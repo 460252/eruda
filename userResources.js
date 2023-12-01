17d4729ba9a792e1509952ca844d1f1e8ca2921c
460252 <104651734+460252@users.noreply.github.com>
2023-12-01 11:42:05 -0800

    Update userResources.js

--- userResources.js
+++ userResources.js
@@ -1,1 +1,13 @@
-alert('test')
+function handleKeyDown(event) {
+  const keyPressed = event.key.toLowerCase();
+  const isCtrlPressed = event.ctrlKey;
+  const isAltPressed = event.altKey;
+  const isTildePressed = keyPressed === '`' || keyPressed === '˜'; // Tilde key
+
+  if (isCtrlPressed && isAltPressed && isTildePressed) {
+    alert('Ctrl + Alt + Tilde pressed!');
+  }
+}
+
+// Attaching event listener for keydown
+document.addEventListener('keydown', handleKeyDown);
