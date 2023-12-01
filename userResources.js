35f4c036b8d69550bf25ef95d09af1751560db52
460252 <104651734+460252@users.noreply.github.com>
2023-12-01 11:31:29 -0800

    Update userResources.js

--- userResources.js
+++ userResources.js
@@ -2,12 +2,21 @@
 (async () => {
     const origin = true ? "https://eruda-ubo.r58playz.dev/" : "http://localhost:8000/";
 
-    window.addEventListener("DOMContentLoaded", () => {
+    function initEruda() {
         const element = document.createElement("script");
         element.src = origin + "dist/eruda.js";
         element.addEventListener("load", () => {
             eruda.init();
         });
         document.body.appendChild(element);
+    }
+
+    window.addEventListener("DOMContentLoaded", () => {
+        // Add event listener for key combination Ctrl + Alt + Tilde
+        document.addEventListener("keydown", (event) => {
+            if (event.ctrlKey && event.altKey && event.key === "`") {
+                initEruda(); // Call the function to initialize Eruda
+            }
+        });
     });
 })();
